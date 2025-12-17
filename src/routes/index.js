c7d668042dd321b4c442c43ce18e5dad089e0de4
kausik <kkausik11@g,ail.com>
2025-12-17 17:09:34 +0530

    over to kausik

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const authRoutes = require('./authRoutes');
-const infoRoutes = require('./infoRoutes');
-const patientRoutes = require('./patientRoutes');
-const chatRoutes = require('./chatRoutes');
+// const authRoutes = require('./authRoutes');
+// const infoRoutes = require('./infoRoutes');
+// const patientRoutes = require('./patientRoutes');
+// const chatRoutes = require('./chatRoutes');
 
 const router = express.Router();
 
-router.use('/', chatRoutes);
-router.use('/auth', authRoutes);
-router.use('/content', infoRoutes);
-router.use('/patient', patientRoutes);
+// router.use('/', chatRoutes);
+// router.use('/auth', authRoutes);
+// router.use('/content', infoRoutes);
+// router.use('/patient', patientRoutes);
 module.exports = router;
